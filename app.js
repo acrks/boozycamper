a2a076a5ff6a19fda3a4e5d0f77224e4286f1921
Alex Crooks <alexcrooks2@gmail.com>
2022-01-18 12:04:30 -0800

    Set up some more route reading for express

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
