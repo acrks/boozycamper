51f33b97e24dc333cc4360890a6c682cd0fbc00d
Alex Crooks <alexcrooks2@gmail.com>
2022-03-18 00:27:43 -0700

    Clean up console logs

--- frontend/src/components/aboutus/aboutus.jsx
+++ frontend/src/components/aboutus/aboutus.jsx
@@ -25,7 +25,6 @@ class AboutUs extends React.Component {
             this.setState({
                 message: this.props.aboutus['text']
             })
-            console.log(this.props)
         }
     }
 
